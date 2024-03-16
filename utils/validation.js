903428aa7ab8042556ce208fa8f4ec539f3a6492
Olga Solodovnikova <ossolodovnikova@yandex.ru>
2024-03-17 01:09:32 +0300

    паттерны валидации емайл и имя для joi

--- utils/validation.js
+++ utils/validation.js
@@ -1,11 +1,12 @@
 const { celebrate, Joi } = require('celebrate');
 
 const urlRegex = /^(https?:\/\/(www\.)?([a-zA-z0-9-]){1,}\.?([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:/?#[]@!\$&'\(\)\*\+,;=])*)/;
-
+const EMAIL_REGEX = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/;
+const NAME_REGEX = /^[а-яА-ЯёЁa-zA-Z\\s-]+$/
 module.exports.signUpValid = celebrate({ // ркгистрация
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30).pattern(NAME_REGEX),
+    email: Joi.string().required().email().pattern(EMAIL_REGEX),
     password: Joi.string().required(),
   }),
 });
